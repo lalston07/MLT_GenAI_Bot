c0e81298ea24056858d45dee118e29b9b6381232
lalston07 <leilaalston07@gmail.com>
2025-07-28 23:59:10 -0400

    update

--- CIK_lookup_module.py
+++ CIK_lookup_module.py
@@ -21,5 +21,26 @@
         (c) return values should at least include CIK, Name, Ticker, but can include more
 """
 
-import numpy as np
-np.log
+import requests
+
+class SecEdgar:
+    def __init__(self, fileurl):
+        self.fileurl = fileurl
+        self.namedict = {}
+        self.tickerdict = {}
+
+        headers = {'user-agent': 'MLT LA leilaalston07@gmail.com'}
+        r = requests.get(self.fileurl, headers=headers)
+
+        self.filejson = r.json
+
+        print(r.text)
+        print(self.filejson)
+
+        self.cik_json_to_dict()
+
+    def cik_json_to_dict(self):
+        self.name_dict = {}
+        self.ticker_dict = {}
+
+se = SecEdgar('https://www.sec.gov/files/company_tickers.json')
